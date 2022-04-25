35a8751d29978c4b95a04cab32866640d8fd8097
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-25 16:53:27 +0300

    feat: Main/Promo

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,12 @@
+import './Promo.css';
+
+function Promo() {
+  return (
+    <div className='promo'>
+      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      <div className='promo__landing-pic' />
+    </div>
+  );
+}
+
+export default Promo;
