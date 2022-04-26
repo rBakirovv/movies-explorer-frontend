2d810da77913c152096f592f07a11528fdbc3490
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-26 17:20:47 +0300

    feat: Main/AboutMe

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,29 @@
+import './AboutMe.css';
+
+function AboutMe() {
+  return (
+    <div className='about-me'>
+      <div className='about-me__title-container'>
+        <h2 className='about-me__title'>Студент</h2>
+      </div>
+      <div className='about-me__main-container'>
+        <div className='about-me__description-container'>
+          <h2 className='about-me__main-title'>Руслан</h2>
+          <p className='about-me__main-subtitle'>Фронтенд-разработчик, 19 лет</p>
+          <p className='about-me__description'>
+            Я родился в Новом Уренгое, затем перебрался учиться в Санкт-Петербург.
+            На этом я не остановлюсь, планирую покорять Москву.
+            Недавно меня заинтерисована веб-разработка и запала в душу. Учась в Яндекс Практикуме, я нашёл свою первую работу.
+          </p>
+          <ul className='about-me__social-list'>
+            <li className='about-me__social-item'>Telegram</li>
+            <li className='about-me__social-item'>Github</li>
+          </ul>
+        </div>
+        <div className='about-me__avatar'></div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
