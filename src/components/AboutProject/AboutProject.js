b0123ba4e7def67950ecee12da59e23a0d386ee3
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-26 16:41:50 +0300

    fix: Main/AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -2,28 +2,28 @@ import './AboutProject.css';
 
 function AboutProject() {
   return (
-    <div className='navtab'>
-      <div className='navtab__title-container'>
-        <h2 className='navtab__title'>О проекте</h2>
+    <div className='about-project'>
+      <div className='about-project__title-container'>
+        <h2 className='about-project__title'>О проекте</h2>
       </div>
-      <div className='navtab__stages'>
-        <div className='navtab__stage-container'>
-          <h3 className='navtab__stage-title'>Дипломный проект включал 5 этапов</h3>
-          <p className='navtab__stage-subtitle'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+      <div className='about-project__stages'>
+        <div className='about-project__stage-container'>
+          <h3 className='about-project__stage-title'>Дипломный проект включал 5 этапов</h3>
+          <p className='about-project__stage-subtitle'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </div>
-        <div className='navtab__stage-container'>
-          <h3 className='navtab__stage-title'>На выполнение диплома ушло 5 недель</h3>
-          <p className='navtab__stage-subtitle'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <div className='about-project__stage-container'>
+          <h3 className='about-project__stage-title'>На выполнение диплома ушло 5 недель</h3>
+          <p className='about-project__stage-subtitle'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </div>
       </div>
-      <div className='navtab__line'>
-        <div className='navtab__line-container'>
-          <p className='navtab__line-step'>1 неделя</p>
-          <p className='navtab__line-step'>4 недели</p>
+      <div className='about-project__line'>
+        <div className='about-project__line-container'>
+          <p className='about-project__line-step'>1 неделя</p>
+          <p className='about-project__line-step'>4 недели</p>
         </div>
-        <div className='navtab__under-line-container'>
-          <p className='navtab__under-line-item'>Back-end</p>
-          <p className='navtab__under-line-item'>Front-end</p>
+        <div className='about-project__under-line-container'>
+          <p className='about-project__under-line-item'>Back-end</p>
+          <p className='about-project__under-line-item'>Front-end</p>
         </div>
       </div>
     </div>
