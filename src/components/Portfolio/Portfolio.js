c7f281003ea5ad17c9d35c0f5bd65060f365a8d8
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-26 18:41:43 +0300

    feat: Main/Portfolio

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,23 @@
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className='portfolio'>
+      <h2 className='portfolio__title'>Портфолио</h2>
+      <a href='https://rbakirovv.github.io/how-to-learn/' target='_blank' rel='noreferrer' className='portfolio__project-container'>
+        <p className='portfolio__project-title'>Статичный сайт</p>
+        <div className='portfolio__link'></div>
+      </a>
+      <a href='https://rbakirovv.github.io/russian-travel/' target='_blank' rel='noreferrer' className='portfolio__project-container'>
+        <p className='portfolio__project-title'>Адаптивный сайт</p>
+        <div className='portfolio__link'></div>
+      </a>
+      <a href='https://bakirov.students.nomoredomains.work/' target='_blank' rel='noreferrer' className='portfolio__project-container'>
+        <p className='portfolio__project-title'>Одностраничное приложение</p>
+        <div className='portfolio__link'></div>
+      </a>
+    </div>
+  );
+}
+
+export default Portfolio;
