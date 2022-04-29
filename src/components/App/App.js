015bf11e72050627508be209743013eee098f927
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-29 22:43:40 +0300

    fix: router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Main from '../Main/Main';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-      <Switch>
-        <Route path='/'>
-          <Header loggedIn={false} />
-          <Main />
-          <Footer />
-        </Route>
-      </Switch>
+    <Routes>
+      <Route path='/' element={<Main />} />
+    </Routes>
   );
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,15 +5,19 @@ import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
 import Portfolio from '../Portfolio/Portfolio';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function Main() {
   return (
     <>
+      <Header />
       <Promo />
       <AboutProject />
       <Techs />
       <AboutMe />
       <Portfolio />
+      <Footer />
     </>
   );
 }
