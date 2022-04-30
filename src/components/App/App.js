00329cd1aaf6d6103cb3375af47a8b4e77dff1d8
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-30 19:51:53 +0300

    feat: Profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Main from '../Main/Main';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from '../NotFound/NotFound';
+import Profile from '../Profile/Profile';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/profile' element={<Profile />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './Profile.css';
+
+function Profile() {
+
+  return (
+    <>
+      <Header loggedIn={true} />
+      <section className='profile'>
+        <h2 className='profile__title'>Привет, Руслан!</h2>
+        <form className='profile__form'>
+          <label className='profile__label' htmlFor='name'>Имя</label>
+          <input className='profile__input' value='Руслан' type='text' id='name' />
+          <label className='profile__label' htmlFor='email'>E-mail</label>
+          <input className='profile__input' value='email@mail.ru' type='email' id='email' />
+        </form>
+        <div className='profile__buttons-container'>
+          <button className='profile__account-button' type='button'>Редактировать</button>
+          <button className='profile__account-button' type='button'>Выйти из аккаунта</button>
+        </div>
+        <div className='profile__edit-container'>
+          <span className='profile__save-button-span'>При обновлении профиля произошла ошибка.</span>
+          <button className='profile__save-button profile__save-button_disabled' type='button'>Сохранить</button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Profile;
