2d9ca65cec6c7f1811509fe45b749564a48fc0de
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-04-30 16:06:20 +0300

    feat: 404 page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Main from '../Main/Main';
 import { Route, Routes } from 'react-router-dom';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
