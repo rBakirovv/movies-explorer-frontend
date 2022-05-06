a1dda2876e64891af6f9d82fa9239b782b0356b4
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-05-06 14:49:40 +0300

    feat: UserContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Main from '../Main/Main';
 import { Route, Routes } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import NotFound from '../NotFound/NotFound';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
@@ -9,16 +10,21 @@ import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState({});
+  
   return (
-    <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='/signup' element={<Register />} />
-      <Route path='/signin' element={<Login />} />
-      <Route path='/movies' element={<Movies />} />
-      <Route path='/saved-movies' element={<SavedMovies />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <CurrentUserContext.Provider value={currentUser}>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/signup' element={<Register />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Register.css'
 
+/* /^[А-ЯA-ZёәіңғүұқөһӘІҢҒҮҰҚӨҺ\h-]+$/umi */
+
 function Register() {
   return (
     <section className='register'>
